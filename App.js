0f402f8dcc52ef3013a4d13ac66d03b74342da92
RAJPATEL-DROID <raj1729patel@gmail.com>
2023-06-12 23:36:16 +0530

    Lecture 3 : React Element  Component in JSX

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading1 = React.createElement("h1", { id: "title" }, "Namste React");
 
-const heading2 = React.createElement("h1", { id: "title" }, "Namste React");
+// React Element
+// const heading = React.createElement(
+//   "h1",
+//   { id: "title", key: "h2" },
+//   "Namste React"
+// );
 
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading1, heading2]
+// JSX Variable
+// const heading = (
+//   <h1 id="title" key="h1">
+//     Namste React
+//   </h1>
+// );
+
+// React Component
+// - Functional Component - NEW
+const Heading = () => (
+  <h1 id="title" key="h1">
+    Namste React
+  </h1>
 );
 
+const HeaderComponent = () => {
+  return (
+    <div>
+      {/* You can write any piece of javascript here  */ Heading()}
+      <Heading />
+      <h1>Namster React</h1>
+      <h2>This is Just H2 tag</h2>
+    </div>
+  );
+};
+
+// - Class Based Component - OLD
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<HeaderComponent />);
